dc31c2d61b45cf3be06e78b4039728f3a9125f7b
ricardorompar <ricardo.romero@hashicorp.com>
2025-05-12 18:24:39 +0200

    removed example import

--- import-resources/imported.tf
+++
@@ -1,9 +0,0 @@
-# File genertated automatically by script
-# Always check the generated file or plan before running 'terraform apply'
-
-import {
-  to = vsphere_virtual_machine.database-server-01
-  id = vm-10659
-}
-
-# End of generated file
